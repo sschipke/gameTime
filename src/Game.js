0f675d5996a2aa1ba826b6f9a20ec054d4e0b559
Amy Rippeto <amyrippeto@gmail.com>
2019-08-31 15:19:29 -0600

    Add methods and tests for game class

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,7 @@
 import data from './data.js';
 import Player from './Player.js';
+import Round from './Round.js';
+import FastMoney from './FastMoney.js';
 
 class Game {
   constructor(surveys, answers) {
@@ -7,6 +9,9 @@ class Game {
     this.answers = answers;
     this.players = []; 
     this.roundCounter = 0;
+    this.currentSurvey;
+    this.currentAnswers;
+    this.usedSurveys = [0];
   }
 
   addPlayers(p1, p2) {
@@ -14,6 +19,37 @@ class Game {
     let player2 = new Player(2, p2);
     this.players.push(player1, player2);
   }
+
+  selectSurvey() {
+    let randomNum = Math.round(Math.random() * this.surveys.length);
+    if (this.usedSurveys.includes(randomNum)) {
+      this.selectSurvey();
+    } else {
+    this.currentSurvey = this.surveys.find(survey => survey.id === randomNum);
+    this.usedSurveys.push(randomNum);
+    }
+  }
+
+  getSurveyAnswers() {
+    this.selectSurvey();
+    this.currentAnswers = this.answers.filter(answer => answer.surveyId === this.currentSurvey.id);
+    this.currentAnswers.sort((a, b) => b.respondents - a.respondents);   
+  }
+
+  startRound() {
+    this.getSurveyAnswers();
+    if (this.roundCounter <= 2) {
+      let round = new Round(this.currentSurvey, this.currentAnswers, this.players);
+      this.roundCounter++;
+      // console.log(round)
+    } else {
+      let fastMoney = new FastMoney(this.currentSurvey, this.currentAnswers, this.players);
+      this.roundCounter++;
+    }
+  }
+
+
+
 }
 
 export default Game;
--- test/Game-test.js
+++ test/Game-test.js
@@ -2,7 +2,9 @@ const expect = chai.expect;
 import chai from 'chai';
 
 import Game from '../src/Game.js';
-import data from '../src/data.js'
+import data from '../src/data.js';
+
+// import Round from '../src/Round.js';
 
 describe('Game', () => {
   let game;
@@ -20,6 +22,27 @@ describe('Game', () => {
     expect(game.players[1].name).to.equal('Scott')
   })
 
+  it('should store used surveys', () => {
+    game.selectSurvey();
+    game.selectSurvey();
+    expect(game.usedSurveys.length).to.equal(3);
+  })
 
+  it('should get the current survey answers', () => {
+    game.getSurveyAnswers();
+    expect(game.currentAnswers.length).to.eql(3);
+  })
+
+  // it('should be able to start a round', () => {
+  //   game.startRound();
+  //   expect(game.round.answers.length).to.equal(3)
+
+  // })
+
+  it('should keep track of the current round', () => {
+    game.startRound();
+    game.startRound();
+    expect(game.roundCounter).to.equal(2)
+  })
 
 })
