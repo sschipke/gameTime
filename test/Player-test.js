8b18d53e7ed9246df4da891483f82f1840b67bd0
Quinne Farenwald <q.farenwald@gmail.com>
2019-08-31 13:36:21 -0600

    Create player class and tests

---
+++ test/Player-test.js
@@ -0,0 +1,40 @@
+const expect = chai.expect;
+import chai from 'chai';
+
+import Player from '../src/Player.js';
+
+
+describe('Player', () => {
+
+  let player;
+
+  beforeEach(() => {
+    player = new Player(1, 'Quinne');
+  });
+
+  it('should be a function', () => {
+    expect(Player).to.be.a('function');
+  });
+
+  it('should be an instance of Player', () => {
+    expect(player).to.be.an.instanceof(Player);
+  });
+
+  it('should have an id', () => {
+    expect(player.id).to.equal(1);
+  });
+
+  it('should have a name', () => {
+    expect(player.name).to.equal('Quinne');
+  });
+
+  it('should have a score', () => {
+    expect(player.score).to.equal(0);
+  });
+
+  it('should update the score', () => {
+    player.updateScore(25);
+    expect(player.score).to.equal(25);
+  });
+
+});
